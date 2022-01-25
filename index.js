fc87651243afd4958987ba3f5fd846a932c2a39f
figmentdragon <figmentdragon@hotmail.com>
2022-01-25 04:21:28 -0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var sass = import("sass");
 var Fiber = import("fibers");
 var fa = import("fontawesome");
-var result = sass.renderSync({file:
-    entry: path.resolve(__dirname, "assets/scripts/scss/style.scss");
 
 sass.render({
   file: "input.scss",
